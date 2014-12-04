0a61d54e496095ebf4b0619eca425c373ca5d717
egig <egigundari@gmail.com>
2014-12-05 01:31:31 +0700

    index

--- README.md
+++
@@ -1,4 +0,0 @@
-notify
-======
-
-Simple jquery plugin gives little bit animation to bootstrap alert. Forked from UIKit Notify add-on
---
+++ js/demo.js
@@ -0,0 +1,8 @@
+(function(){
+
+	$('.notifier').on('click', function(e){
+		e.preventDefault();
+		$.notify('&hellip; dead simple jquery plugin gives little bit animation to bootstrap alert. Like this. Forked UIKit Notify add-on');
+	});
+
+})(jQuery);
